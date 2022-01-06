7a6b99367a8326c3dd6883b982e502a019fc98ae
Özcan Çaparoğlu <ozcan.caparoglu@sahabt.com>
2022-01-06 12:51:23 +0300

    Application and Ioc preparations

---
+++ src/Services/Contacts/Contact.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace Contact.Application.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            
+        }
+    }
+}
---
+++ src/Services/Contacts/Contact.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,24 @@
+﻿using Contact.Application.Contracts.Persistence;
+using Contact.Infrastructure.Persistence;
+using Contact.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Contact.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ContactContext>(options =>
+                options.UseNpgsql(configuration.GetConnectionString("ContactConnectionString")));
+
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+            services.AddScoped<IContactPersonRepository, ContactPersonRepository>();
+            services.AddScoped<IContactPersonInfoRepository, ContactPersonInfoRepository>();
+
+            return services;
+        }
+    }
+}
